e1481551f0b4585c1e45f65c45f119d9f9ba86f7
Asfami Ansharie <asfami.ansharie@bni.co.id>
2025-02-26 11:06:30 +0700

    Fixing Soal 5

--- ConsoleApp2/Soal 5.cs
+++ ConsoleApp2/Soal 5.cs
@@ -9,29 +9,11 @@ namespace ConsoleApp2
         public List<int> ProcessFinalNumbers(List<int> angkaList)
         {
             Console.WriteLine($"Hasil angka dari Soal 4: {string.Join(" ", angkaList)}");
-            List<int> hasilAkhir = new List<int>();
-
-           
-            for (int i = 0; i < angkaList.Count; i++)
-            {
-                int angka = angkaList[i];
-
-                if (angka == 0) 
-                {
-                    hasilAkhir.Add(1);
-                }
-                else if (angka % 2 == 0) 
-                {
-                    hasilAkhir.Add(angka + 1);
-                }
-                else 
-                {
-                    hasilAkhir.Add(angka);
-                }
-            }
+            
+            List<int> hasilAkhir = angkaList.Select(angka => 
+                    (angka == 0) ? 1 : (angka % 2 == 0) ? angka + 1 : angka).ToList();
             
             Console.WriteLine($"Hasil transformasi angka setelah koreksi: {string.Join(" ", hasilAkhir)}");
-
             return hasilAkhir;
         }
     }
